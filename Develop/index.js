1dbc576f1c2db37a5d6085a1436c00f8dee72854
Apo11o <jeffknoespel@gmail.com>
2021-08-01 20:04:44 -0500

    capture title input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
+const { writeToFile } = require('./utils/generateMarkdown.js')
+const inquirer = require('inquirer');
+generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
